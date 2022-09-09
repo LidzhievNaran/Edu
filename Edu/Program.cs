f421444953bba3653a2d1f2df2d6f7fe4ce8a18b
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-09-10 02:38:29 +0300

    data

--- Edu/Program.cs
+++ Edu/Program.cs
@@ -6,14 +6,19 @@ namespace Edu
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("=> Data type Parsing:");
-            bool b = bool.Parse("True");
-            Console.WriteLine("Value of b: {0}", b);
-            double d = double.Parse("2312");// не хочет работать с десятичными
-            Console.WriteLine("Value of d: {0}", d);
-            int c = int.Parse("1000");
-            Console.WriteLine("Value of c: {0}", c);
-            
+            Console.WriteLine("=> Dates and Times:"); 
+            //этот конструктор принимает годб месяц и день
+            DateTime dt = new DateTime (2015, 10, 17);
+            //какой это день месяца?
+            Console.WriteLine("The day of {0} is {1}", dt.Date, dt.DayOfWeek);
+            //Сейчас месяц декабрь
+            dt = dt.AddMonths(2); 
+            Console.WriteLine("Daylight savings: {0}", dt.IsDaylightSavingTime());
+            //Этот конструктор принимает часы, минуты и секунды
+            TimeSpan ts = new TimeSpan(4, 30, 0) ;
+            Console.WriteLine(ts);
+            //Вычесть 15 минут из текущего значения TimeSpan и вывести результат
+            Console.WriteLine(ts.Subtract (new TimeSpan(0, 15, 0)));
         }
     }
 }
