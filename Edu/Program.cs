d2dcccabf29337f04d2fa12c885fd6be566c5296
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-09-10 02:19:07 +0300

    Initial commit

---
+++ Edu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Edu
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            char myChar = 'a';
+            Console.WriteLine("char.isDigit ('a'): {0}", char.IsDigit(myChar));
+            Console.WriteLine("char.isLetter ('a'): {0}", char.IsLetter(myChar));
+            
+        }
+    }
+}
