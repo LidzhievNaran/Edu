79cdc500c1cf09176527c345d76fdd8d36d66d78
LidzhievNaran <lidzhievnaran483@gmail.com>
2022-09-10 02:21:38 +0300

    white space and punctuations

--- Edu/Program.cs
+++ Edu/Program.cs
@@ -6,10 +6,16 @@ namespace Edu
     {
         public static void Main(string[] args)
         {
-            char myChar = 'a';
+            char myChar = 'f';
             Console.WriteLine("char.isDigit ('a'): {0}", char.IsDigit(myChar));
             Console.WriteLine("char.isLetter ('a'): {0}", char.IsLetter(myChar));
-            
+            Console.WriteLine("char.IsWhiteSpace('Hello There’, 5): {0}",
+                char.IsWhiteSpace("Hello There", 5));
+            Console.WriteLine("char.IsWhiteSpace('Hello There', 6): {0}",
+                char.IsWhiteSpace("Hello There", 6));
+            Console.WriteLine("char.IsPunctuation(’?'): {0}",
+                char.IsPunctuation('?'));
+            Console.WriteLine();
         }
     }
 }
